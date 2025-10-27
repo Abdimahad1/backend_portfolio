b2a87620e525e841ca0054e33f38fe0de3838489
Abdimahad1 <mucjisoduusho123@gmail.com>
2025-10-27 16:39:41 +0300

    Use onboarding@resend.dev for testing emails

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -5,7 +5,7 @@ console.log('SMTP User:', process.env.SMTP_USER);
 console.log('From email:', process.env.DEFAULT_FROM_EMAIL);
 
 // Create Gmail transporter
-const transporter = nodemailer.createTransporter({
+const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST || 'smtp.gmail.com',
   port: process.env.SMTP_PORT || 587,
   secure: process.env.SMTP_SECURE === 'true',
